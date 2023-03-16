b43a56bd48d048e345824993b7d130f3fe6fe3b1
Kristaps31 <kristaps.v@hotmail.co.uk>
2023-03-16 17:14:56 +0000

    This commit contains code for task7, the function to upwote review

--- src/componenets/CommentCard.jsx
+++ src/componenets/CommentCard.jsx
@@ -1,12 +1,12 @@
 import "../componenets_CSS/CommentsCard.css"
 
-const CommentCard = (comment) => {
+const CommentCard = ({comment}) => {
   return (
-    <section className="commentsCard">
-   <p className="div1">Comment by: {comment.comment.author}</p>
-   <p className="div2">Posted: {comment.comment.created_at}</p>
-   <p className="div3">{comment.comment.body}</p>
-   <p className="div4">Votes: {comment.comment.votes}</p>
+    <section key={comment.comment_id} className="commentsCard">
+   <p className="div1">Comment by: {comment.author}</p>
+   <p className="div2">Posted: {comment.created_at}</p>
+   <p className="div3">{comment.body}</p>
+   <p className="div4">Votes: {comment.votes}</p>
     </section>
   )
 }
